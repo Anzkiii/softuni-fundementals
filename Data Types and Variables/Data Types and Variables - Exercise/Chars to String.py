6aff1cd98b1cc358b55422bff490a23501958fb1
Edward <edi.tzekov@gmail.com>
2022-05-31 23:01:33 +0300

    some more changes

---
+++ Data Types and Variables/Data Types and Variables - Exercise/Chars to String.py
@@ -0,0 +1,6 @@
+
+charOne = input()
+charTwo = input()
+charThree = input()
+
+print(f"{charOne}{charTwo}{charThree}")
---
+++ Data Types and Variables/Data Types and Variables - Exercise/Integer Operations.py
@@ -0,0 +1,7 @@
+from math import floor
+firstInt = int(input())
+secondInt = int(input())
+thirdInt = int(input())
+fourthInt = int(input())
+x = ((firstInt + secondInt) / thirdInt) * fourthInt  
+print(floor(x))
