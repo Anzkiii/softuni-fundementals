5e8c778e1335092df7c6da99e13bd717d65126b0
Edward <edi.tzekov@gmail.com>
2022-06-17 19:58:27 +0300

    Update Football_Cards.py

--- Lists Basics/Lists Basics - Exercise/Football_Cards.py
+++ Lists Basics/Lists Basics - Exercise/Football_Cards.py
@@ -1,4 +1,19 @@
 
-teamA = []
-teamB = []
+A = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+B = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
+A_left = []
+B_left = []
+playerCards = input()
+playerCardsLst = playerCards.split(" ")
 
+for i in playerCardsLst:
+    if "A" in i and i[2:] not in A_left:
+        A.remove(i[2:])
+        A_left.append(i[2:]) 
+    elif "B" in i and i[2:] not in B_left:
+        B_left.append(i[2:]) 
+        B.remove(i[2:])
+if len(A) < 7 or len(B) < 7:
+    print(f"Team A - {len(A)}; Team B - {len(B)}\nGame was terminated")
+else:
+    print(f"Team A - {len(A)}; Team B - {len(B)}")
